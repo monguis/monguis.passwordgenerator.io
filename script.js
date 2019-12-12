dd1406f50de48564ae59040f1978429f5991a3c3
monguis <jccnnetwork@gmail.com>
2019-12-11 18:57:44 -0700

    linked function and button in html

--- script.js
+++ script.js
@@ -7,11 +7,13 @@ var LowCharArray = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", 
 var UprCharArray = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M"]
 var passwordLength = 0;
 var generatedpassword = "";
+var passgenclick = document.getElementById("passGenButton")
 
 
-passgen();
+// passgenclick.onclick = passgen();
 
-function passgen() {
+function passgen(idAux) {
+    document.getElementById(idAux).value ="";
     passwordLength = 0;
     generatedpassword = "";
     var count = 0;
@@ -59,4 +61,7 @@ function passgen() {
     } else {
         alert("Please use a valid number")
     }
+
+    document.getElementById(idAux).value = (generatedpassword);
 }
+
