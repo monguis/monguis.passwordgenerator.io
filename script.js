ba189e6bcfd62e805e0ba1680ac65695528e8796
monguis <jccnnetwork@gmail.com>
2019-12-13 09:54:23 -0700

    deleted some unnecessery variables

--- script.js
+++ script.js
@@ -7,16 +7,11 @@ var LowCharArray = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", 
 var UprCharArray = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M"]
 var passwordLength = 0;
 var generatedpassword = "";
-var passgenclick = document.getElementById("passGenButton")
-
-
-// passgenclick.onclick = passgen();
 
 function passgen(idAux) {
     document.getElementById(idAux).value = "";
     passwordLength = 0;
     generatedpassword = "";
-    var count = 0;
     passwordLength = prompt("How many characters do you want your password to have? (8-128 characters allowed)");
 
     var auxarray = [];
